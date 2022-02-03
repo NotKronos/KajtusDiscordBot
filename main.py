f3b535c9290cf5d57c72590e03ea74675cce4756
TukannAdam <adasmik2017@gmail.com>
2022-02-03 13:23:10 +0100

    nsfw

--- main.py
+++ main.py
@@ -11,5 +11,12 @@ client = commands.Bot(command_prefix="$")
 async def ping(ctx):
     await ctx.message.channel.send('Pong!')
 
+@client.command(name="cycki")
+async def cycki(ctx):
+    if ctx.message.channel.is_nsfw():
+        await ctx.message.channel.send(nekos.img("boobs"))
+    else:
+        await ctx.message.channel.send("Aby użyć tej komendy kanał musi być oznaczony jako NSFW")
+
 
 client.run(token)
